8e6aa6f3bbddabc9deb6e47e27eb1e4bde8d3d5b
yunhye-choi <yunhye_choi@tmax.co.kr>
2023-12-29 19:26:20 +0900

    [feat] HTTP 요청 파라미터 - @RequestParam의 다양한 사용법, 필수여부 지정, default값 지정, Map으로 받기

--- src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -5,8 +5,11 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -18,5 +21,69 @@ public class RequestParamController {
 
         log.info("username={}, age={}", username, age);
         response.getWriter().write("ok");
+    }
+
+    @ResponseBody // : return값을 응답 바디에 박아넣음 (@RestController와 같은 기능)
+    @RequestMapping("/request-param-v2")
+    public String requestParamV2(
+            @RequestParam("username") String memberName,
+            @RequestParam("age") int memberAge
+    ){
+        log.info("username={}, age={}", memberName, memberAge);
+
+        return "ok";
+    }
+
+    @ResponseBody // : return값을 응답 바디에 박아넣음 (@RestController와 같은 기능)
+    @RequestMapping("/request-param-v3")
+    public String requestParamV3(
+            @RequestParam String username,
+            @RequestParam int age
+    ){
+        log.info("username={}, age={}", username, age);
+
+        return "ok";
+    }
+
+    @ResponseBody // : return값을 응답 바디에 박아넣음 (@RestController와 같은 기능)
+    @RequestMapping("/request-param-v4")
+    public String requestParamV4(String username, int age){
+        log.info("username={}, age={}", username, age);
+
+        return "ok";
+    }
+
+    @ResponseBody // : return값을 응답 바디에 박아넣음 (@RestController와 같은 기능)
+    @RequestMapping("/request-param-required")
+    public String requestParamRequired(
+            @RequestParam(required = false) String username,
+            @RequestParam(required = true) int age
+    ){
+        log.info("username={}, age={}", username, age);
+
+        return "ok";
+    }
+
+    @ResponseBody // : return값을 응답 바디에 박아넣음 (@RestController와 같은 기능)
+    @RequestMapping("/request-param-default")
+    public String requestParamDefault(
+            @RequestParam(required = false, defaultValue = "user") String username,
+            @RequestParam(required = true, defaultValue = "-1") int age
+    ){
+        log.info("username={}, age={}", username, age);
+
+        return "ok";
+    }
+
+    @ResponseBody // : return값을 응답 바디에 박아넣음 (@RestController와 같은 기능)
+    @RequestMapping("/request-param-map")
+    public String requestParamMap(
+            @RequestParam Map<String, Object> paramMap){
+
+        String username = (String) paramMap.get("username");
+        int age = Integer.parseInt((String)paramMap.get("age"));
+        log.info("username={}, age={}", username, age);
+
+        return "ok";
     }
 }
